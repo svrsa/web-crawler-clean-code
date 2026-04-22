0a7b4777fa46dd00c192e6ee58d8c4760e18c2fa
Stefan <stefan.vrs99@gmail.com>
2026-04-22 22:21:18 +0200

    feat: parse crawler arguments and print startup configuration

--- src/main/java/at/aau/webcrawler/ArgumentParser.java
+++ src/main/java/at/aau/webcrawler/ArgumentParser.java
@@ -1,7 +1,17 @@
 package at.aau.webcrawler;
 
+import java.util.List;
+
 public class ArgumentParser {
   public CrawlerConfiguration parse(String[] args) {
-    return null;
+    if (args.length != 3) {
+      throw new IllegalArgumentException("Expected exactly 3 arguments: <url> <depth> <domains>");
+    }
+
+    String startUrl = args[0];
+    int maxDepth = Integer.parseInt(args[1]);
+    List<String> allowedDomains = List.of(args[2].split(","));
+
+    return new CrawlerConfiguration(startUrl, maxDepth, allowedDomains);
   }
 }
--- src/main/java/at/aau/webcrawler/Main.java
+++ src/main/java/at/aau/webcrawler/Main.java
@@ -1,5 +1,12 @@
 package at.aau.webcrawler;
 
 public class Main {
+  public static void main(String[] args) {
+    ArgumentParser argumentParser = new ArgumentParser();
+    CrawlerConfiguration configuration = argumentParser.parse(args);
 
+    System.out.println("URL: " + configuration.getStartUrl());
+    System.out.println("Depth: " + configuration.getMaxDepth());
+    System.out.println("Domains: " + configuration.getAllowedDomains());
+  }
 }
