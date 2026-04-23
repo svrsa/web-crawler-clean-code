95222636bf4c132d8010fa0fc0169972acd13fda
Stefan <stefan.vrs99@gmail.com>
2026-04-23 11:55:37 +0200

    feat: load and parse a single webpage

--- src/main/java/at/aau/webcrawler/Main.java
+++ src/main/java/at/aau/webcrawler/Main.java
@@ -1,12 +1,26 @@
 package at.aau.webcrawler;
 
+import org.jsoup.nodes.Document;
+
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
     ArgumentParser argumentParser = new ArgumentParser();
     CrawlerConfiguration configuration = argumentParser.parse(args);
 
+    CrawlerService crawlerService = new CrawlerService();
+    HtmlParser htmlParser = new HtmlParser();
+
+    Document document = crawlerService.loadDocument(configuration.getStartUrl());
+
+    List<String> headings = htmlParser.extractHeadings(document);
+    List<String> links = htmlParser.extractLinks(document);
+
     System.out.println("URL: " + configuration.getStartUrl());
     System.out.println("Depth: " + configuration.getMaxDepth());
     System.out.println("Domains: " + configuration.getAllowedDomains());
+    System.out.println("Headings: " + headings);
+    System.out.println("Links: " + links);
   }
 }
