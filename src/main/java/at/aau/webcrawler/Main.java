b1a10ad4ac0a40dbac4d59efdbd23f1406b50363
Stefan <stefan.vrs99@gmail.com>
2026-04-23 12:20:18 +0200

    refactor: simplify main startup flow

--- src/main/java/at/aau/webcrawler/Main.java
+++ src/main/java/at/aau/webcrawler/Main.java
@@ -1,26 +1,19 @@
 package at.aau.webcrawler;
 
-import org.jsoup.nodes.Document;
-
-import java.util.List;
-
 public class Main {
   public static void main(String[] args) {
     ArgumentParser argumentParser = new ArgumentParser();
     CrawlerConfiguration configuration = argumentParser.parse(args);
 
     CrawlerService crawlerService = new CrawlerService();
-    HtmlParser htmlParser = new HtmlParser();
+    PageResult pageResult = crawlerService.analyzePage(
+        configuration.getStartUrl(),
+        configuration.getMaxDepth()
+    );
 
-    Document document = crawlerService.loadDocument(configuration.getStartUrl());
-
-    List<String> headings = htmlParser.extractHeadings(document);
-    List<String> links = htmlParser.extractLinks(document);
-
-    System.out.println("URL: " + configuration.getStartUrl());
-    System.out.println("Depth: " + configuration.getMaxDepth());
-    System.out.println("Domains: " + configuration.getAllowedDomains());
-    System.out.println("Headings: " + headings);
-    System.out.println("Links: " + links);
+    System.out.println("URL: " + pageResult.getUrl());
+    System.out.println("Depth: " + pageResult.getDepth());
+    System.out.println("Headings: " + pageResult.getHeadings());
+    System.out.println("Links: " + pageResult.getLinks());
   }
 }
