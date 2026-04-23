3f04c287fb2b78d5b2350416a24da993361dee01
Stefan <stefan.vrs99@gmail.com>
2026-04-23 11:47:09 +0200

    feat: add document loading to crawler service

--- src/main/java/at/aau/webcrawler/CrawlerService.java
+++ src/main/java/at/aau/webcrawler/CrawlerService.java
@@ -1,5 +1,17 @@
 package at.aau.webcrawler;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class CrawlerService {
 
+  public Document loadDocument(String url) {
+    try {
+      return Jsoup.connect(url).get();
+    } catch (IOException exception) {
+      throw new IllegalArgumentException("Could not load document from URL: " + url, exception);
+    }
+  }
 }
