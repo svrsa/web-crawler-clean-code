193e6b537ce1dd66f65d8cd652418fa14249442e
Stefan <stefan.vrs99@gmail.com>
2026-04-27 12:49:01 +0200

    fix: add missing report write exception

---
+++ src/main/java/at/aau/webcrawler/writer/ReportWriteException.java
@@ -0,0 +1,7 @@
+package at.aau.webcrawler.writer;
+
+public class ReportWriteException extends RuntimeException {
+  public ReportWriteException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
